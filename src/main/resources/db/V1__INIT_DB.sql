b4429542df90390b1e82b04999a1ca415bc8ae24
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-01-31 09:36:54 +0100

    Test adding table

--- src/main/resources/db/V1__INIT_DB.sql
+++ src/main/resources/db/V1__INIT_DB.sql
@@ -1,10 +1,21 @@
 DO
 $$
 BEGIN
-        IF EXISTS
-            (select 1 from pg_roles where rolname = 'cloudsqliamuser')
-        THEN
-            grant all on all tables in schema public to cloudsqliamuser;
-END IF;
+    IF EXISTS
+        (select 1 from pg_roles where rolname = 'cloudsqliamuser')
+    THEN
+        grant all on all tables in schema public to cloudsqliamuser;
+    END IF;
 END
 $$;
+
+CREATE TABLE consent(
+    id BIGSERIAL CONSTRAINT consent_pk PRIMARY KEY,
+    title TEXT,
+    responsible_group TEXT,
+    purpose TEXT,
+    total_involved BIGINT,
+    expiration DATE,
+    code TEXT
+);
+-- Add references to candidate and employee
