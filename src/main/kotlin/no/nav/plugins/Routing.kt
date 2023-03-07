5d0d7f54ce21c75bd55dd5bf27f8bf6192aeb922
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-03-07 10:38:10 +0100

    Move

--- src/main/kotlin/no/nav/plugins/Routing.kt
+++ src/main/kotlin/no/nav/plugins/Routing.kt
@@ -1,5 +1,7 @@
 package no.nav.plugins
 
+import com.auth0.jwk.JwkProvider
+import com.auth0.jwk.JwkProviderBuilder
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
@@ -9,6 +11,7 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.routing.*
 import io.ktor.http.*
+import io.ktor.server.response.*
 import no.nav.ApplicationContext
 import no.nav.isNais
 import no.nav.routes.citizenRoute
@@ -16,6 +19,7 @@ import no.nav.routes.employeeRoute
 import no.nav.routes.healthRoute
 import no.nav.routes.mocks.citizenRouteMock
 import no.nav.routes.mocks.employeeRouteMock
+import java.util.concurrent.TimeUnit
 
 fun Application.configureRouting() {
     install(ContentNegotiation) { json() }
@@ -28,6 +32,45 @@ fun Application.configureRouting() {
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
         allowMethod(HttpMethod.Put)
+    }
+
+    val azureADProvider: JwkProvider = JwkProviderBuilder(System.getenv("AZURE_OPENID_CONFIG_JWKS_URI"))
+        .cached(10, 24, TimeUnit.HOURS)
+        .rateLimited(10, 1, TimeUnit.MINUTES)
+        .build()
+
+    val tokenXProvider: JwkProvider = JwkProviderBuilder(System.getenv("TOKEN_X_JWKS_URI"))
+        .cached(10, 24, TimeUnit.HOURS)
+        .rateLimited(10, 1, TimeUnit.MINUTES)
+        .build()
+
+    if (isNais()) {
+        install(Authentication) {
+            jwt("citizen") {
+                println("Got here")
+                verifier(tokenXProvider, System.getenv("TOKEN_X_ISSUER"))
+                challenge { _, _ -> call.respond(HttpStatusCode.Unauthorized) }
+                validate { cred ->
+                    println(cred)
+                    if (!cred.audience.contains(System.getenv("TOKEN_X_CLIENT_ID"))) {
+                        println("Audience does not match!")
+                        return@validate null
+                    }
+                    JWTPrincipal(cred.payload)
+                }
+            }
+            jwt("employee") {
+                verifier(azureADProvider, System.getenv("AZURE_OPENID_CONFIG_ISSUER"))
+                challenge { _, _ -> call.respond(HttpStatusCode.Unauthorized) }
+                validate { cred ->
+                    if (!cred.audience.contains(System.getenv("AZURE_APP_CLIENT_ID"))) {
+                        println("Audience does not match!")
+                        return@validate null
+                    }
+                    JWTPrincipal(cred.payload)
+                }
+            }
+        }
     }
 
     routing {
