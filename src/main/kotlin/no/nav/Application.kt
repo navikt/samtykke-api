c6a1353a6c9999e8a92331908f29320eeb785d87
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-03-07 18:24:22 +0100

    Remove

--- src/main/kotlin/no/nav/Application.kt
+++ src/main/kotlin/no/nav/Application.kt
@@ -25,24 +25,22 @@ fun Application.module() {
         .build()
 
     val tokenXProvider: JwkProvider = JwkProviderBuilder(System.getenv("TOKEN_X_JWKS_URI"))
-        .cached(10, 24, TimeUnit.HOURS)
-        .rateLimited(10, 1, TimeUnit.MINUTES)
         .build()
 
     if (isNais()) {
         install(Authentication) {
             jwt("citizen") {
                 verifier(tokenXProvider, System.getenv("TOKEN_X_ISSUER"))
-                challenge { _, _ ->
-                    call.respond(HttpStatusCode.Unauthorized)
-                }
                 validate { cred ->
-                    //if (!cred.audience.contains(System.getenv("TOKEN_X_CLIENT_ID"))) {
-                    //    println("Audience does not match!")
-                    //    return@validate null
-                    //}
+                    if (!cred.audience.contains(System.getenv("TOKEN_X_CLIENT_ID"))) {
+                        println("Audience does not match!")
+                        return@validate null
+                    }
                     JWTPrincipal(cred.payload)
                 }
+//                challenge { _, _ ->
+//                    call.respond(HttpStatusCode.Unauthorized)
+//                }
             }
             jwt("employee") {
                 verifier(azureADProvider, System.getenv("AZURE_OPENID_CONFIG_ISSUER"))
