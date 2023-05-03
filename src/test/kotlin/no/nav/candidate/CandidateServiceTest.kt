d04d8f739c775c8d9282960f6e1f9a529059a5cf
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-05-03 14:35:00 +0200

    Add tests for updating candidate

--- src/test/kotlin/no/nav/candidate/CandidateServiceTest.kt
+++ src/test/kotlin/no/nav/candidate/CandidateServiceTest.kt
@@ -5,6 +5,7 @@ import io.mockk.*
 import kotlinx.datetime.LocalDate
 import no.nav.database.dao.CandidateDao
 import no.nav.database.dao.ConsentDao
+import no.nav.models.Candidate
 import no.nav.models.CandidateStatus
 import no.nav.models.CreateCandidateRequest
 import no.nav.services.CandidateService
@@ -97,5 +98,122 @@ internal class CandidateServiceTest {
         }
 
         verify(exactly = 0) { candidateDao.createCandidature(any(), any(), any()) }
+    }
+
+    @Test
+    fun `able to anonymize candidate`() {
+        every { consentDao.getOwnerIdByConsentId(any()) }.returns("")
+        every { consentDao.getConsentByCode(any()) }.returns(
+            mockk {
+                every { id }.returns(1)
+                every { title }.returns("Brukertest")
+                every { responsibleGroup }.returns("Team ReOps")
+                every { theme }.returns("skjemaer")
+                every { purpose }.returns("teste veldig fine skjemaer")
+                every { totalInvolved }.returns(2)
+                every { expiration }.returns(LocalDate(2023, 6, 12))
+                every { endResult }.returns("rapport")
+                every { slackChannelId }.returns("")
+                every { code }.returns("XX1-XX2")
+                every { candidates }.returns(null)
+                every { employee }.returns(null)
+            }
+        )
+        every { candidateDao.anonymizeCandidate(any(), any()) }.returns(mockk())
+        every { candidateDao.getCitizenCandidature(any(), any()) }.returns(
+            mockk { every { trackingNumber }.returns("xxx") }
+        )
+
+        assertDoesNotThrow {
+            candidateService.anonymizeCandidate("", "")
+        }
+
+        verify(exactly = 1) { candidateDao.getCitizenCandidature(any(), any()) }
+        verify(exactly = 1) { candidateDao.anonymizeCandidate(any(), any()) }
+    }
+
+    @Test
+    fun `able to update valid candidate`() {
+        every { consentDao.getOwnerIdByConsentId(any()) }.returns("")
+        every { consentDao.getConsentByCode(any()) }.returns(
+            mockk {
+                every { id }.returns(1)
+                every { title }.returns("Brukertest")
+                every { responsibleGroup }.returns("Team ReOps")
+                every { theme }.returns("skjemaer")
+                every { purpose }.returns("teste veldig fine skjemaer")
+                every { totalInvolved }.returns(2)
+                every { expiration }.returns(LocalDate(2023, 6, 12))
+                every { endResult }.returns("rapport")
+                every { slackChannelId }.returns("")
+                every { code }.returns("XX1-XX2")
+                every { candidates }.returns(null)
+                every { employee }.returns(null)
+            }
+        )
+        every { candidateDao.getCitizenCandidature(any(), any()) }.returns(
+            mockk { every { trackingNumber }.returns("xxx") }
+        )
+        every { candidateDao.updateCandidate(any(), any(), any()) }.returns(mockk())
+
+        val updatedCandidate = Candidate(
+            1,
+            "Nussi nøff",
+            "nussi.noef@gmail.com",
+            CandidateStatus.ACCEPTED,
+            null,
+            "xx",
+            true,
+            1
+        )
+
+        assertDoesNotThrow {
+            candidateService.updateCandidate(updatedCandidate, "", "")
+        }
+
+        verify(exactly = 1) { candidateDao.getCitizenCandidature(any(), any()) }
+        verify(exactly = 1) { candidateDao.updateCandidate(any(), any(), any()) }
+    }
+
+    @Test
+    fun `unable to update in-valid candidate`() {
+        every { consentDao.getOwnerIdByConsentId(any()) }.returns("")
+        every { consentDao.getConsentByCode(any()) }.returns(
+            mockk {
+                every { id }.returns(1)
+                every { title }.returns("Brukertest")
+                every { responsibleGroup }.returns("Team ReOps")
+                every { theme }.returns("skjemaer")
+                every { purpose }.returns("teste veldig fine skjemaer")
+                every { totalInvolved }.returns(2)
+                every { expiration }.returns(LocalDate(2023, 6, 12))
+                every { endResult }.returns("rapport")
+                every { slackChannelId }.returns("")
+                every { code }.returns("XX1-XX2")
+                every { candidates }.returns(null)
+                every { employee }.returns(null)
+            }
+        )
+        every { candidateDao.getCitizenCandidature(any(), any()) }.returns(
+            mockk { every { trackingNumber }.returns("xxx") }
+        )
+
+        val invalidUpdatedCandidate = Candidate(
+            1,
+            "",
+            "",
+            CandidateStatus.ACCEPTED,
+            null,
+            "xx",
+            false,
+            1
+        )
+
+        assertThrows<BadRequestException> {
+            candidateService.updateCandidate(invalidUpdatedCandidate, "", "")
+        }
+
+        verify(exactly = 0) { candidateDao.getCitizenCandidature(any(), any()) }
+        verify(exactly = 0) { candidateDao.updateCandidate(any(), any(), any()) }
     }
 }
