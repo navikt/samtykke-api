20b05d3f20ad3ba0f8adb48871af7a3a718e3183
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-05-05 12:07:26 +0200

    Fix pdfgen requests to not use internal ktor api

--- src/main/kotlin/no/nav/citizen/CitizenRoute.kt
+++ src/main/kotlin/no/nav/citizen/CitizenRoute.kt
@@ -1,7 +1,6 @@
 package no.nav.citizen
 
 import io.ktor.client.*
-import io.ktor.client.call.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -9,7 +8,6 @@ import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.util.*
 import no.nav.candidate.Candidate
 import no.nav.getCitizenId
 import no.nav.consent.pdf.generateConsentPDF
@@ -18,7 +16,6 @@ import no.nav.candidate.CreateCandidateRequest
 import no.nav.consent.ConsentService
 import no.nav.consent.pdf.PDFVersion
 
-@OptIn(InternalAPI::class)
 fun Route.citizenRoute(
     consentService: ConsentService,
     candidateService: CandidateService,
@@ -74,7 +71,7 @@ fun Route.citizenRoute(
                     val code = call.parameters["code"].toString()
                     val consent = consentService.getConsentByCodeWithCandidate(code, getCitizenId(call.principal(), citizenService))
                     val response = generateConsentPDF(httpClient, PDFVersion.CITIZEN, consent, consent.employee!!, consent.candidates!![0])
-                    call.respond(response.content)
+                    call.respond(response.readBytes())
                 }
             }
         }
